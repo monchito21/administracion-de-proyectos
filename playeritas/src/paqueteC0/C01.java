b267f781a64824f54d561f99ae6a215d5bd0fbe6
CarlozWayne <legacy43@hotmail.com>
2015-03-24 14:45:34 -0600

    inicio de sesión Beta 2.0

--- playeritas/src/paqueteC0/C01.java
+++ playeritas/src/paqueteC0/C01.java
@@ -1,16 +1,23 @@
 package paqueteC0;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class C01 extends JFrame{
+    private JButton i;
+    private JTextField A1;
+    private JPasswordField A2;
+    private final JFrame frame;
     public C01(){
         setSize(400,180);
         setTitle("Playeritas online");
@@ -18,6 +25,7 @@ public class C01 extends JFrame{
         componentes();
         setIconImage(new ImageIcon("logo.png").getImage());
         con();
+        frame = this;
     }
     public void componentes(){
         JPanel centro = new JPanel();
@@ -27,22 +35,34 @@ public class C01 extends JFrame{
         centro.setLayout(new BoxLayout(centro, BoxLayout.PAGE_AXIS));
         JLabel user = new JLabel("Usuario");
         JLabel pass = new JLabel("Contraseña");
-        JTextField A1 = new JTextField(15);
-        JTextField A2 = new JTextField(15);
+        A1 = new JTextField(15);
+        A2 = new JPasswordField(15);
         centro.add(user);
         centro.add(A1);
         centro.add(pass);
         centro.add(A2);
         
         //sur
-        JButton i = new JButton("Iniciar");
+        i = new JButton("Iniciar");
         sur.add(i,BorderLayout.CENTER);
         }
     public void con(){
         /*
         
-        Conexion en construcción
+           Conexion en construcción
         
         */
+        i.addActionListener(new ActionListener(){
+            @Override 
+            public void actionPerformed(ActionEvent e){
+                if(A1.getText().equals("") || A2.getText().equals("")){
+                    JOptionPane.showMessageDialog(null,"Faltan campos por llenar");
+                }
+                else{
+                    interfaz in = new interfaz(frame);
+                    in.setVisible(true);
+                }
+            }
+        });
     }
 }
---
+++ playeritas/src/paqueteC0/interfaz.java
@@ -0,0 +1,76 @@
+package paqueteC0;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class interfaz extends javax.swing.JFrame {
+    public interfaz(JFrame j) {
+        j.dispose();
+        initComponents();
+        setIconImage(new ImageIcon("logo2.png").getImage()); 
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Nuevo");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 437, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    // End of variables declaration//GEN-END:variables
+}
